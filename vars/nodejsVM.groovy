41102e31a53210bfeed0c13a8d86dcdde559584d
PavanKalyan49r <pavan49r@gmail.com>
2024-04-25 16:15:10 +0530

    jenkins

--- vars/nodejsVM.groovy
+++ vars/nodejsVM.groovy
@@ -7,7 +7,7 @@ pipeline {
     }
     environment {
        packageVersion =''
-       nexusUrl = '172.31.29.16:8081'
+       nexusUrl = '172.31.16.72:8081'
     }
     options {
         timeout(time:1, unit: 'HOURS')
